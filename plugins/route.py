57f1a9b8f0ceb172f82ac3afdbf149dcafd6344d
Bhimsinh Sodha <118103528+bhimsinh3@users.noreply.github.com>
2022-11-28 15:33:35 +0530

    Create route.py

---
+++ plugins/route.py
@@ -0,0 +1,13 @@
+# @bjsodha is professional bot Developer 
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("The Legend @annoying_boy")
